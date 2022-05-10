8706327b7db3c9846518c4be1babbea508e4b4bc
Akhilanand Kundurthi <kundurthiakhil@gmail.com>
2022-05-09 23:54:06 -0500

    Modified footer
    While performing UI testing, I found that there is an incorrect footer that is being displayed—I modified the footer accordingly.

---
+++ src/components/Reuseable/Footer.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export default function Footer() {
+    return (
+        <footer>
+            <div className="footer-block"></div>
+                <div className="container-fluid">
+                    <div className="row">
+                        <div className="col-12 pt-2 mx-auto text-center bg-dark text-light">
+                            <h6>© 2022 Coursedo Inc. All rights reserved.</h6>
+                        </div>
+                    </div>
+                </div>
+
+        </footer>
+    )
+}
